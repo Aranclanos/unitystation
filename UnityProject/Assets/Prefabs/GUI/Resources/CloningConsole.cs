a076087a1c34678602f21b499e5bcc1a5025451b
Aranclanos <aranclanos@hotmail.com>
2019-05-01 05:29:47 -0300

    Adds the cloning system control UI for the cloning computer

---
+++ UnityProject/Assets/Prefabs/GUI/Resources/CloningConsole.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloningConsole : NetworkTabTrigger
+{
+	public override bool Interact(GameObject originator, Vector3 position, string hand)
+	{
+		if (!CanUse(originator, hand, position, false))
+		{
+			return false;
+		}
+		if (!isServer)
+		{
+			//ask server to perform the interaction
+			InteractMessage.Send(gameObject, position, hand);
+			return true;
+		}
+
+		TabUpdateMessage.Send(originator, gameObject, NetTabType, TabAction.Open);
+
+		return true;
+	}
+}
---
+++ UnityProject/Assets/Prefabs/GUI/Resources/GUI_Cloning.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUI_Cloning : NetTab
+{
+
+	public void Autoprocess()
+	{
+		Debug.Log("ARAN: autoprocess");
+	}
+
+	public void StartScan()
+	{
+
+	}
+
+	public void LockScanner()
+	{
+
+	}
+
+	public void ViewRecords()
+	{
+
+	}
+
+	public void ViewSpecificRecord()
+	{
+
+	}
+
+	public void Back()
+	{
+
+	}
+
+	public void DeleteRecord()
+	{
+
+	}
+
+	public void Clone()
+	{
+
+	}
+
+	public void DEBUGTURNOFF()
+	{
+
+	}
+
+
+
+}
