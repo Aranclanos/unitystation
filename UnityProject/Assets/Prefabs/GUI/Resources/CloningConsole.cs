f73b2b977415befeceadbb64193162ac372bfdea
Aranclanos <aranclanos@hotmail.com>
2019-05-04 04:18:31 -0300

    Adds CloningRecords for the cloning console to use

--- UnityProject/Assets/Prefabs/GUI/Resources/CloningConsole.cs
+++ UnityProject/Assets/Prefabs/GUI/Resources/CloningConsole.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CloningConsole : NetworkTabTrigger
 {
+	public delegate void ChangeEvent();
+	public static event ChangeEvent changeEvent;
+
+	public List<CloningRecord> CloningRecords = new List<CloningRecord>();
+
 	public override bool Interact(GameObject originator, Vector3 position, string hand)
 	{
 		if (!CanUse(originator, hand, position, false))
@@ -17,8 +22,49 @@ public class CloningConsole : NetworkTabTrigger
 			return true;
 		}
 
+		UpdateGUI();
 		TabUpdateMessage.Send(originator, gameObject, NetTabType, TabAction.Open);
 
 		return true;
 	}
+
+	public void Start(){
+		//DEBUG
+		CloningRecord CRone = new CloningRecord("Dana Ray", "7602", 50, 117, 0, 33, "630990308360514eb1815");
+		CloningRecords.Add(CRone);
+		CloningRecord CRtwo = new CloningRecord("Trinity Ray", "3465", 50, 117, 0, 33, "630991235560514Eb1815");
+		CloningRecords.Add(CRtwo);
+	}
+
+	public void UpdateGUI()
+	{
+		// Change event runs updateAll in CloningGUI
+		if (changeEvent != null)
+		{
+			changeEvent();
+		}
+	}
 }
+
+public class CloningRecord
+{
+	public string Name;
+	public string ScanID;
+	public float OxyDmg;
+	public float BurnDmg;
+	public float ToxingDmg;
+	public float BruteDmg;
+	public string UniqueIdentifier;
+
+	public CloningRecord(string name, string scanID, float oxyDmg, float burnDmg, float toxingDmg, float bruteDmg, string uniqueIdentifier)
+	{
+		Name = name;
+		ScanID = scanID;
+		OxyDmg = oxyDmg;
+		BurnDmg = burnDmg;
+		ToxingDmg = toxingDmg;
+		BruteDmg = bruteDmg;
+		UniqueIdentifier = uniqueIdentifier;
+	}
+}
+
--- UnityProject/Assets/Prefabs/GUI/Resources/GUI_Cloning.cs
+++ UnityProject/Assets/Prefabs/GUI/Resources/GUI_Cloning.cs
@@ -5,50 +5,110 @@ using UnityEngine.UI;
 
 public class GUI_Cloning : NetTab
 {
+	public CloningConsole CloningConsole;
+
+	public int CurrentScreen = 1;
+
+	public GameObject AllScreens;
+	public GameObject MainScreen;
+	public GameObject RecordListScreen;
+	public GameObject SpecificRecordScreen;
+
+
+	void Start()
+	{
+		if (Provider != null)
+		{
+			//Makes sure it connects with the dispenser properly
+			CloningConsole = Provider.GetComponentInChildren<CloningConsole>();
+			//Subscribe to change event from CloningConsole.cs
+			CloningConsole.changeEvent += UpdateDisplay;
+			UpdateDisplay();
+		}
+	}
+
+
+	public void UpdateDisplay()
+	{
+		if(CurrentScreen == 0)
+		{
+			AllScreens.SetActive(false);
+		}
+		if (CurrentScreen == 1)
+		{
+			RecordListScreen.SetActive(false);
+			SpecificRecordScreen.SetActive(false);
+			MainScreen.SetActive(true);
+			AllScreens.SetActive(true); //DEBUG: for power switch in the black screen
+		}
+		if (CurrentScreen == 2)
+		{
+			MainScreen.SetActive(false);
+			RecordListScreen.SetActive(true);
+		}
+		if (CurrentScreen == 3)
+		{
+			MainScreen.SetActive(false);
+			SpecificRecordScreen.SetActive(true);
+		}
+	}
+
+	public void OnDestroy()
+	{
+		//Unsubscribe container update event
+		CloningConsole.changeEvent -= UpdateDisplay;
+	}
 
 	public void Autoprocess()
 	{
 		Debug.Log("ARAN: autoprocess");
+		UpdateDisplay();
 	}
 
 	public void StartScan()
 	{
-
+		Debug.Log("ARAN: scanning");
+		UpdateDisplay();
 	}
 
 	public void LockScanner()
 	{
-
+		Debug.Log("ARAN: lock toggled");
+		UpdateDisplay();
 	}
 
 	public void ViewRecords()
 	{
-
+		CurrentScreen = 2;
+		UpdateDisplay();
 	}
 
 	public void ViewSpecificRecord()
 	{
-
+		CurrentScreen = 3;
+		UpdateDisplay();
 	}
 
 	public void Back()
 	{
-
+		CurrentScreen = 1;
+		UpdateDisplay();
 	}
 
 	public void DeleteRecord()
 	{
-
+		UpdateDisplay();
 	}
 
 	public void Clone()
 	{
-
+		UpdateDisplay();
 	}
 
 	public void DEBUGTURNOFF()
 	{
-
+		CurrentScreen = 0;
+		UpdateDisplay();
 	}
 
 
